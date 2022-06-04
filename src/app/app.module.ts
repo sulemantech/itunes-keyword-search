332d528c090a9127818180f7f62a5b774e30cb5d
Sulman <shahsuliman@gmail.com>
2022-06-04 16:03:10 +0500

    Created the iTunes Keyword search angualr project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ITunesSearchComponent } from './i-tunes-search/i-tunes-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ITunesSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/i-tunes-search/i-tunes-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-i-tunes-search',
+  templateUrl: './i-tunes-search.component.html',
+  styleUrls: ['./i-tunes-search.component.css']
+})
+export class ITunesSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
